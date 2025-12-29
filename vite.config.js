cc6a44771bd6551c3bee3a0410bade422d96ae99
atish363 <sauravshahi363@gmail.com>
2025-12-29 12:20:18 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base:"home-tutor",
+  base:"/home-tutor",
   plugins: [react()],
   server: {
     port: 3000,
